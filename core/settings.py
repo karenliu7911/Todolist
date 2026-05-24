963317bc8a8853463a69aa390f1fb9e4d4eaefa3
karen <karen@gmail.com>
2026-05-24 12:05:38 +0800

    完成模型建立跟後台檢視

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
+    "todos",
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,7 @@ TIME_ZONE = "Asia/Taipei"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
